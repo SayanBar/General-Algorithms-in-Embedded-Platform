8fddc2cbcf2d3f4158f3ce550e190ed30a48a054
SayanBar <sayan.barman@colorado.edu>
2018-04-28 00:32:09 -0600

    code_3

---
+++ removeDupString.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+#include <stdlib.h>
+#define FAIL 0
+#define ASCII_SIZE 256
+
+char * removDup(char *str);
+
+int main(void) {
+	char str[]="xxxxxyyyyyyyyyyzzzzzzzzzzzzzzz32453543wwwwqwqweeccee%%###**";
+	char *str1=removDup(str);
+	printf("%s",str1);
+	return 0;
+}
+
+char* removDup(char *str){
+    uint8_t count[ASCII_SIZE]={0};
+    char *finalStr=malloc(sizeof(char)*strlen(str));
+    uint8_t counter=0;
+    uint8_t finalCount=0;
+    while(counter<strlen(str)){
+        //printf("Counter:%d\n",counter);
+        if(count[str[counter]]==0){
+            count[str[counter]]++;
+            finalStr[finalCount++]=str[counter];
+            
+        }
+        counter++;
+            
+            
+    }
+    
+    
+    return finalStr;
+}
