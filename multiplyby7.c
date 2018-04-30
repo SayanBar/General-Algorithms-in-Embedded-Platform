7fbb343c8f71e4a64119a5b150840ed5d7fdf335
SayanBar <sayan.barman@colorado.edu>
2018-04-30 12:57:12 -0600

    code_18

---
+++ multiplyby7.c
@@ -0,0 +1,38 @@
+//Multiply a number by 7
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+#include <stdlib.h>
+#define FAIL 0
+#define ASCII_SIZE 10
+
+
+
+# include<stdio.h>
+ 
+int16_t multiplyBySeven(int16_t n)
+{  
+    uint8_t negFlag=0;
+    if(n<0){
+        n=-n;
+        negFlag=1;
+    }
+    /* Note the inner bracket here. This is needed 
+       because precedence of '-' operator is higher 
+       than '<<' */
+    int16_t res=((n<<3) - n);
+    if(negFlag==1)
+        return (-res);
+    else
+        return res;
+}
+ 
+/* Driver program to test above function */
+int main()
+{
+    int16_t n = 5;
+    printf("%d", multiplyBySeven(n));
+    return 0;
+}
+
