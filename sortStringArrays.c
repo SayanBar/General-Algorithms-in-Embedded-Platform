9568b9422ca61a39ddf2fda74db4e35dfcd1a629
SayanBar <sayan.barman@colorado.edu>
2018-05-08 12:42:19 -0600

    code_80 Sort strings in an Array

---
+++ sortStringArrays.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+ 
+static int myCompare (const void * a, const void * b)
+{
+    return strcmp (*(const char **) a, *(const char **) b);
+}
+ 
+void sort(const char *arr[], int n)
+{
+    qsort (arr, n, sizeof (const char *), myCompare);
+}
+ 
+int main ()
+{
+    const char *arr[] = {"GeeksforGeeks", "GeeksQuiz", "CLanguage"};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int i;
+ 
+    printf("Given array is\n");
+    for (i = 0; i < n; i++)
+        printf("%d: %s \n", i, arr[i]);
+ 
+    sort(arr, n);
+ 
+    printf("\nSorted array is\n");
+    for (i = 0; i < n; i++)
+        printf("%d: %s \n", i, arr[i]);
+ 
+    return 0;
+}
